396696ee53994fab12b0706726dac2dd3b4cf94a
PXgamer <owzie123@gmail.com>
2016-07-26 12:50:40 +0100

    Created a maintenance mode message page

---
+++ maintenance.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head itemscope itemtype="http://schema.org/WebSite">
+		<meta name="viewport" content="initial-scale=1.0,width=device-width">
+		<meta name="HandheldFriendly" content="true"/>
+		<title>KatRevive [Maintenance]</title>
+		<link rel="shortcut icon" href="/favicon.png">
+		<meta name="theme-color" content="#594c2d">
+		<style>
+		  body { text-align: center; padding: 150px; font: 20px Helvetica, sans-serif; color: #333; }
+		  h1 { font-size: 50px; }
+		  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+		  a { color: #594c2d; text-decoration: none; }
+		  a:hover { color: #333; text-decoration: none; }
+		</style>
+	</head>
+	<body>
+		<article>
+			<h1>We’ll be back soon!</h1>
+			<div>
+				<p>Sorry for any inconvenience, but KatRevive is currently undergoing maintenance. Please check back soon!</p>
+				<p>— <a href="https://pxgamer.github.io/KatRevive/">The KatRevive Team</a></p>
+			</div>
+		</article>
+	</body>
+</html>
