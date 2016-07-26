0475778436d19788d7b076cf02b785e04a482d2a
PXgamer <owzie123@gmail.com>
2016-07-26 16:42:41 +0100

    Changed maintenance page to flexbox
    Changed maintenance page to flexbox for better mobile responsiveness.
    Fix for #12

--- maintenance.php
+++ maintenance.php
@@ -7,19 +7,34 @@
 		<link rel="shortcut icon" href="/favicon.png">
 		<meta name="theme-color" content="#594c2d">
 		<style>
-		  body { text-align: center; padding: 150px; font: 20px Helvetica, sans-serif; color: #333; }
-		  h1 { font-size: 50px; }
-		  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-		  a { color: #594c2d; text-decoration: none; }
-		  a:hover { color: #333; text-decoration: none; }
+			body { text-align: center; font: 20px Helvetica, sans-serif; color: #333; }
+			h1 { font-size: 50px; }
+			.box {
+			  display: -ms-flexbox;
+			  display: -webkit-flex;
+			  display: flex;
+			  
+			  -webkit-justify-content: center;
+			  justify-content: center;
+			  margin: 50px 50px;
+			}
+			.box-inner {
+			  flex: 0 1 auto;
+			  align-self: auto;
+			}
+			article { display: block; text-align: left; }
+			a { color: #594c2d; text-decoration: none; }
+			a:hover { color: #333; text-decoration: none; }
 		</style>
 	</head>
 	<body>
-		<article>
-			<h1>We'll be back soon!</h1>
-			<div>
-				<p>Sorry for any inconvenience, but KatRevive is currently undergoing maintenance. Please check back soon!</p>
-				<p>- <a href="https://pxgamer.github.io/KatRevive/">The KatRevive Team</a></p>
+		<article class="box">
+			<div class="box-inner">
+				<h1>We'll be back soon!</h1>
+				<div>
+					<p>Sorry for any inconvenience, but KatRevive is currently undergoing maintenance. Please check back soon!</p>
+					<p>- <a href="https://pxgamer.github.io/KatRevive/">The KatRevive Team</a></p>
+				</div>
 			</div>
 		</article>
 	</body>
