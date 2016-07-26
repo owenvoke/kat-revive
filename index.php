cfb6869df0fbece1967977580637e04b42b14319
PXgamer <owzie123@gmail.com>
2016-07-26 09:21:52 +0100

    Added global head file and fixed issue #10

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head itemscope itemtype="http://schema.org/WebSite">
-		<meta charset="UTF-8">
-		<meta name="viewport" content="initial-scale=1.0,width=device-width">
-		<meta name="HandheldFriendly" content="true"/>
-		<title>KatRevive</title>
-		<link rel="shortcut icon" href="favicon.png">
-		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-		<script async src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
-		<script async src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script>
-	</head>
+	<?php include('head.php'); ?>
 	<body>
 		<?php
 		include('funcs.php');
