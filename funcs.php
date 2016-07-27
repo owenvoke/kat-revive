e06e06f40d8a6d5b3bf2410642acd86099badc5f
PXgamer <owzie123@gmail.com>
2016-07-27 12:34:24 +0100

    Refined the install process and made it easier to use
    Install process now checks the following:
    - Whether the mysqli extension is functioning
    - Whether there is access to the db using the default vars
    
    If access is not allowed, then it will open a form to submit your new
    details.
    
    This will modify the funcs.php file to enable the new db details.

--- funcs.php
+++ funcs.php
@@ -5,7 +5,7 @@
 	{
 
 		public static function conn() {
-			/* VARIABLES */
+			/* MySQL VARIABLES */
 			$db_serv = "localhost";
 			$db_user = "root";
 			$db_pass = "";
@@ -17,5 +17,38 @@
 		public static function query($db_conn, $sql) {
 			return mysqli_query ($db_conn, $sql);
 		}
+		
+		public static function changeDbDetails($mysql_host = "localhost", $mysql_username = "root", $mysql_password = "", $mysql_dbname = "kat_db") {
+			
+			// Open funcs.php file for editing
+			$fname = "funcs.php";
+			$content = file_get_contents($fname);
+			
+			// Replace with new details
+			$content = preg_replace(
+				[
+					'/\$db_serv = "(.*)"/',
+					'/\$db_user = "(.*)"/',
+					'/\$db_pass = "(.*)"/',
+					'/\$db_name = "(.*)"/'
+				],
+				[
+					'$db_serv = "'.$mysql_host.'"',
+					'$db_user = "'.$mysql_username.'"',
+					'$db_pass = "'.$mysql_password.'"',
+					'$db_name = "'.$mysql_dbname.'"'
+				],
+				$content,
+				1
+			);
+			
+			// Write to funcs.php
+			if (file_put_contents($fname, $content)) {
+				return true;
+			}
+			
+			return false;
+		}
+		
 	}
 ?>
