9a9b15f4759873a9c2c6b07fbcfd22fd34bc7588
PXgamer <owzie123@gmail.com>
2016-08-01 23:30:37 +0100

    Fixed minor bug with BS overriding KAT theme colour

--- nav.php
+++ nav.php
@@ -1,19 +1,19 @@
 <?php if (isset($_COOKIE['kat_theme']) && $_COOKIE['kat_theme'] != '') {
     ?>
-<style>
-.nav.navbar-nav li a, .navbar-header .navbar-brand {
-	color: #ffeeb4;
-}
-.nav.navbar-nav li a:hover, .navbar-header .navbar-brand:hover {
-    background: #2c240f;
-	color: #ffeeb4;
-}
-.navbar.navbar-default, .dropdown-menu {
-	background: #594c2d;
-	border:none;
-	border-radius:0;
-}
-</style>
+    <style>
+    .nav.navbar-nav li a, .navbar-header .navbar-brand, .dropdown-menu>li>a {
+    	color: #ffeeb4 !important;
+    }
+    .nav.navbar-nav li a:hover, .navbar-header .navbar-brand:hover, .dropdown-menu>li>a:hover {
+        background: #2c240f !important;
+    	color: #ffeeb4 !important;
+    }
+    .navbar.navbar-default, .dropdown-menu {
+    	background: #594c2d !important;
+    	border:none;
+    	border-radius:0;
+    }
+    </style>
 <?php
 
 } ?>
