5b12cb25d3bbc2c1cfb45bea3eee37b2b354bb53
PXgamer <owzie123@gmail.com>
2016-08-01 11:25:33 +0100

    Updated CRON function to remove dump when count > 5

--- cron/create_dump.php
+++ cron/create_dump.php
@@ -10,4 +10,25 @@ $db_pass 		= '';
 $backup_file 	= "exports/daily_dump_".date("Y-m-d").".sql";
 $cmd			= "mysqldump --insert-ignore -u $db_user -p$db_pass $db_name $tableName > $backup_file";
 exec($cmd);
+
+function scan_dir($dir = "exports") {
+	$ignored = array('.', '..', '.htaccess', 'index.php');
+
+	$files = array();    
+	foreach (scandir($dir) as $file) {
+		if (in_array($file, $ignored)) continue;
+		$files[$file] = filemtime($dir . '/' . $file);
+	}
+
+	arsort($files);
+	$files = array_keys($files);
+
+	return ($files) ? $files : false;
+}
+			
+$dir_files = scan_dir("exports");
+
+if (count($dir_files) > 5) {
+	unlink ("exports/" . $dir_files[6]);
+}
 ?>
