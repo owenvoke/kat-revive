a28aff6d320ddb459609c4fd19c1176e06961d24
Owen Voke <owzie123@gmail.com>
2016-08-03 07:41:31 +0100

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE `kat_db`.`t_collection` (
   `torrent_category` VARCHAR(500) NOT NULL ,
   `torrent_info_url` VARCHAR(1000) NOT NULL ,
   `torrent_download_url` VARCHAR(1000) NOT NULL ,
-  `size` VARCHAR(500) NOT NULL ,
+  `size` BIGINT(100) NOT NULL ,
   `category_id` INT NOT NULL ,
   `files_count` INT NOT NULL ,
   `upload_date` VARCHAR(100) NOT NULL ,
