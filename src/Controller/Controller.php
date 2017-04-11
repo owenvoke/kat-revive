907408f424e347b2762de4c1cfe296d857091c88
PXgamer <owzie123@gmail.com>
2017-04-11 23:22:24 +0100

    Added Smarty loading and db connection to the Controller constructor

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,13 +2,34 @@
 
 namespace pxgamer\KatRevive\Controller;
 
+use pxgamer\KatRevive\Db;
+
+/**
+ * Class Controller
+ * @package pxgamer\KatRevive\Controller
+ */
 class Controller
 {
+    /**
+     * @var \mysqli
+     */
+    private $connection;
+    /**
+     * @var \Smarty
+     */
+    private $smarty;
+
     public function __construct()
     {
         foreach (\System\Request::instance() as $item => $value) {
             $this->$item = $value;
         }
+
+        // Initialise the database connection (uses a singleton)
+        $this->connection = Db::conn();
+
+        // Initialise Smarty config
+        $this->smarty_init();
     }
 
     public function index()
@@ -19,5 +40,12 @@ class Controller
     public function error()
     {
 
+    }
+
+    private function smarty_init()
+    {
+        $this->smarty = new \Smarty();
+        $this->smarty->setTemplateDir(ROOT_PATH . 'templates');
+        $this->smarty->setCompileDir(ROOT_PATH . 'templates_c');
     }
 }
